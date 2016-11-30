b252fbc2ca3384fa6fdbfd2328919b7b50dab8cd
Mack Hasz <mrh17@vt.edu>
2016-11-30 12:34:13 -0500

    aye eee

--- typescript/main.js
+++ typescript/main.js
@@ -6,7 +6,7 @@ editor.session.setOption("useWorker", false)
 editor.setTheme("ace/theme/tomorrow");
 editor.getSession().setMode("ace/mode/typescript");
 
-var ch0 = '\/**\n *  brief overview of the TypeScript language\n **\/\n\nconst enum Color {\n    White,\n    Black,\n    Red,\n    Green,\n    Blue\n}\n\nconst enum Style {\n    Blank,\n    Solid,\n    Stripped,\n    Dotted,\n}\n\ninterface PaintJob {\n    color: Color,\n    style: Style\n}\n\nclass Vehicle {\n    location: number;\n    paintJob: PaintJob;\n    \n    constructor(job?: PaintJob) {\n        if (job) {\n            this.paintJob = {\n                color: Color.White,\n                style: Style.Blank\n            }\n        }\n    }\n    \n    move(distance: number): void {\n        this.location += distance;\n    }\n    \n    paint(job: PaintJob): void {\n        this.paintJob = job;\n    }\n}\n\nclass Car extends Vehicle {\n    private gear: number;\n    \n    constructor() {\n        super({\n                color: Color.Red,\n                style: Style.Stripped\n            });\n        this.gear = 0;\n    }\n    \n    shiftUp(): void {\n        if ( this.gear < 6 ) this.gear++;\n    }\n    \n    shiftDown(): void {\n        if ( this.gear > 0 ) this.gear--;\n    }\n}\n\nclass Plane extends Vehicle {\n    private passengers: number;\n\n    constructor() {\n        super({\n                color: Color.Black,\n                style: Style.Dotted\n            });\n        this.passengers = 0;\n    }\n\n    welcomePassenger(): void {\n        this.passengers++;\n    }\n    \n    planeCrash(): void {\n        this.passengers = 0;\n    }\n}\n\nclass Node<T> {\n    private value : T;\n    \n    public next: Node<T>;\n\n    constructor(v: T, next: Node<T>) {\n        this.setValue(v);\n        this.next = next;\n    }\n    \n    setValue(value : T): void {\n        this.value = value;\n    }\n    \n    getValue(): T {\n        return this.value;\n    }\n}\n\nclass Stack<T> {\n    \n    private head: Node<T>;\n    private size: number;\n    \n    constructor() {\n        this.size = 0;\n    }\n    \n    getSize(): number {\n        return this.size;\n    }\n    \n    isEmpty(): boolean {\n        return this.size == 0;\n    }\n    \n    push( data: T ): void {\n        let node = new Node<T>(data, this.head);\n        this.head = node;\n        this.size++;\n    }\n    \n    pop(): T {\n        if (this.head) {\n            let node = this.head;\n            this.head = this.head.next;\n            this.size--;\n            return node.getValue();\n        } else {\n            return null;\n        }\n    }\n\n    print(): void {\n        let tmp: Node<T> = this.head;\n        while (tmp != null) {\n            console.log(tmp.getValue());\n            tmp = tmp.next;\n        }\n    }\n}\n\nclass PezDispenserGarage {\n    \n    private cars: Stack<Car>;\n    private planes: Stack<Plane>;\n    \n    constructor() {\n        this.cars = new Stack<Car>();\n        this.planes = new Stack<Plane>();\n    }\n}\n\n\/\/ do things';
+var ch0 = '\/**\n *  brief overview of the TypeScript language\n **\/\n\nconst enum Color {\n    White,\n    Black,\n    Red,\n    Green,\n    Blue\n}\n\nconst enum Style {\n    Blank,\n    Solid,\n    Stripped,\n    Dotted,\n}\n\ninterface PaintJob {\n    color: Color,\n    style: Style\n}\n\nclass Vehicle {\n    location: number;\n    paintJob: PaintJob;\n    \n    constructor(job?: PaintJob) {\n        if (job) {\n            this.paintJob = {\n                color: Color.White,\n                style: Style.Blank\n            }\n        }\n    }\n    \n    move(distance: number): void {\n        this.location += distance;\n    }\n    \n    paint(job: PaintJob): void {\n        this.paintJob = job;\n    }\n}\n\nclass Car extends Vehicle {\n    private gear: number;\n    \n    constructor() {\n        super({\n                color: Color.Red,\n                style: Style.Stripped\n            });\n        this.gear = 0;\n    }\n    \n    shiftUp(): void {\n        if ( this.gear < 6 ) this.gear++;\n    }\n    \n    shiftDown(): void {\n        if ( this.gear > 0 ) this.gear--;\n    }\n}\n\nclass Plane extends Vehicle {\n    private passengers: number;\n\n    constructor() {\n        super({\n                color: Color.Black,\n                style: Style.Dotted\n            });\n        this.passengers = 0;\n    }\n\n    welcomePassenger(): void {\n        this.passengers++;\n    }\n    \n    planeCrash(): void {\n        this.passengers = 0;\n    }\n}\n\nclass Node<T> {\n    private value : T;\n    \n    public next: Node<T>;\n\n    constructor(v: T, next: Node<T>) {\n        this.setValue(v);\n        this.next = next;\n    }\n    \n    setValue(value : T): void {\n        this.value = value;\n    }\n    \n    getValue(): T {\n        return this.value;\n    }\n}\n\nclass Stack<T> {\n    \n    private head: Node<T>;\n    private size: number;\n    \n    constructor() {\n        this.size = 0;\n    }\n    \n    getSize(): number {\n        return this.size;\n    }\n    \n    isEmpty(): boolean {\n        return this.size == 0;\n    }\n    \n    push( data: T ): void {\n        var node = new Node<T>(data, this.head);\n        this.head = node;\n        this.size++;\n    }\n    \n    pop(): T {\n        if (this.head) {\n            var node = this.head;\n            this.head = this.head.next;\n            this.size--;\n            return node.getValue();\n        } else {\n            return null;\n        }\n    }\n\n    print(): void {\n        var tmp: Node<T> = this.head;\n        while (tmp != null) {\n            console.log(tmp.getValue());\n            tmp = tmp.next;\n        }\n    }\n}\n\nclass PezDispenserGarage {\n    \n    private cars: Stack<Car>;\n    private planes: Stack<Plane>;\n    \n    constructor() {\n        this.cars = new Stack<Car>();\n        this.planes = new Stack<Plane>();\n    }\n}\n\n\/\/ do things';
 editor.setValue(ch0, -1);
 
 /* compile is triggered off of run button and Ctrl-ENTER or Mac Cmd-ENTER */
@@ -83,7 +83,7 @@ function transpileModule(input, options) {
         }
     });
 
-    let emitResult = program.emit();
+    var emitResult = program.emit();
     if (outputText === undefined) {
         throw new Error("Output generation failed");
     }
@@ -93,16 +93,16 @@ function transpileModule(input, options) {
         ... for some reason JS lib headers are not recognized 
     */
 
-    let allDiagnostics = ts.getPreEmitDiagnostics(program).concat(emitResult.diagnostics);
-    let annotations = [];
+    var allDiagnostics = ts.getPreEmitDiagnostics(program).concat(emitResult.diagnostics);
+    var annotations = [];
 
     allDiagnostics.forEach(function(diagnostic) {
         if (diagnostic.start != null) {
-            let {
+            var {
                 line,
                 character
             } = diagnostic.file.getLineAndCharacterOfPosition(diagnostic.start);
-            let message = ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n');
+            var message = ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n');
 
             if (!message.includes('console') && !message.includes('NaN') && !message.includes('document') &&
                 !message.includes('Array') && !message.includes('map') && !message.includes('push') && 
